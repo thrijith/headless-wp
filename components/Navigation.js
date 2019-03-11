150764b75f665d989dbc4558f4c4108788533a34
Thrijith Thankachan <thrijith13@gmail.com>
2019-03-11 19:20:27 +0530

    Add style to navigation component

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,8 +1,24 @@
 import Link from 'next/link'
 
 export default () => (
-	<ul>
-		<li><Link href="/"><a href="/">Home</a></Link></li>
-		<li><Link href="/posts"><a href="/posts">Posts</a></Link></li>
-	</ul>
+	<div id="navigation">
+		<Link href="/"><a href="/">Home</a></Link>
+		<Link href="/posts"><a href="/posts">Posts</a></Link>
+		<style jsx>{`
+#navigation {
+  overflow: hidden;
+  background-color: #333;
+}
+
+#navigation a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+    `}</style>
+	</div>
 )
