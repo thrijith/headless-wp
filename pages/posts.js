1ef4fbe2b4d5fd4372bf7a4a3d93112fea920932
Thrijith Thankachan <thrijith13@gmail.com>
2019-03-11 15:38:11 +0530

    Add navigation component Add / Update pages to use navigation component

--- pages/posts.js
+++ pages/posts.js
@@ -4,6 +4,6 @@ import { Fragment } from 'react'
 export default () => (
 	<Fragment>
 		<Navigation/>
-		<h1>Your new server-side rendered React.js app!</h1>
+		<h1>Our Posts Page!</h1>
 	</Fragment>
 )
